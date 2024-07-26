d5462e1f0abe0c563b8dd1373f0828beb76666d1
elly sitoya <sitoyaelly@gmail.com>
2024-07-26 13:21:50 +0300

    Added cors for the client

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,46 @@
+// const express = require("express");
+// const connectDB = require("./db/connectDB");
+// require("dotenv").config();
+// const formRouter = require("./routes/submitForm");
+// const app = express();
+// const port = 5000;
+
+// app.use(express.json());
+// app.get("/", (req, res) => res.send("Hello World!"));
+// app.use("/form", formRouter);
+
+// const start = () => {
+//   try {
+//     connectDB(process.env.MONGO_URI);
+//     app.listen(port, () =>
+//       console.log(`Example app listening on port ${port}!`)
+//     );
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// start();
+// -----------------------------------------------------------------
 const express = require("express");
+const cors = require("cors"); // Import cors
 const connectDB = require("./db/connectDB");
 require("dotenv").config();
 const formRouter = require("./routes/submitForm");
+
 const app = express();
 const port = 5000;
 
+const corsOptions = {
+  origin: "http://127.0.0.1:5500",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
+
 app.get("/", (req, res) => res.send("Hello World!"));
 app.use("/form", formRouter);
 
